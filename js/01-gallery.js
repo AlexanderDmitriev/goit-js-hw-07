e565231a5a8808488dd70737c8a0445aee2d65df
AlexanderDmitriev <alex911007@inbox.ru>
2021-12-11 00:12:37 +0200

    Задание 1 в процессе

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
+/* Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+
+    1) Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+    2) Реализация делегирования на div.gallery и получение url большого изображения.
+    3) Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr 
+и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+    4) Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+    5) Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку 
+модального окна с изображением из примеров библиотеки basicLightbox.
+ */
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const imageFromGallery = galleryItems.map(image => `<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+    <img class="gallery__link"
+      class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+ </a>
+</div>`).join('');
+galleryList.insertAdjacentHTML('afterBegin', imageFromGallery);
+
+
+
+const imagesHandler = (event) => {
+    console.log(event.target.dataset.source);
+ }
+
+
+const galleryListener = document.querySelector('.gallery');
+galleryListener.addEventListener('click', imagesHandler);
+
